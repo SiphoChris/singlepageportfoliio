b9f9830be19ca7d8cc8788a023692e5809781bf9
SiphoChris <siphonkebe@gmail.com>
2024-06-26 11:37:27 +0200

    app.vue -update

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,7 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-  
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-=======
 import NavBar from "@/components/NavBar.vue";
->>>>>>> 7ec6e0e0f3e98b351e2182427980f2bac0a7e366
 
 export default {
   components:{
